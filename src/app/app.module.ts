b2e4f1bcbfcc9b009cdf86fb7056ff3d504a23e8
KyunginNa <nakyungin93@gmail.com>
2024-03-10 01:39:32 +0100

    chore: revert to initial application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 
-@NgModule({
-  imports: [BrowserModule, CommonModule],
-  declarations: [AppComponent],
-  providers: [],
-  bootstrap: [AppComponent],
-})
-export class AppModule {}
 
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
+@NgModule({
+    imports: [
+        BrowserModule,
+        CommonModule
+    ],
+    declarations: [
+        AppComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
